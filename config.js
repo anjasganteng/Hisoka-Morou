b476b36e95a09b47343c4bdde4dca02dcabbf157
anjasganteng <102732642+anjasganteng@users.noreply.github.com>
2022-07-26 09:11:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6285876531805','6285876531805','6285876531805']
+global.premium = ['6285876531805']
+global.packname = 'AnjkLu'
+global.author = 'IG : anjasnovixd'
+global.sessionName = 'IG : anjasnovixd'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
